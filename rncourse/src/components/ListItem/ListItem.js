a607fb095131aa74f68359f97eb201822d03a1a7
Shane Honanie <shaneh425@gmail.com>
2018-12-18 12:45:22 -0800

    Static Image

--- rncourse/src/components/ListItem/ListItem.js
+++ rncourse/src/components/ListItem/ListItem.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
 const ListItem = props => (
 	<TouchableOpacity onPress={props.onItemPressed}>
-		<View style={styles.listItem} onPress={props.onItemPressed}>
+		<View style={styles.listItem}>
+			<Image
+				resizeMode="cover"
+				source={props.placeImage}
+				style={styles.placeImage}
+			/>
 			<Text>{props.placeName}</Text>
 		</View>
 	</TouchableOpacity>
@@ -14,7 +19,14 @@ const styles = StyleSheet.create({
 		width: '100%',
 		padding: 10,
 		marginBottom: 5,
-		backgroundColor: '#eee'
+		backgroundColor: '#eee',
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
+	placeImage: {
+		marginRight: 8,
+		height: 30,
+		width: 30
 	}
 });
 
