8ddef3cc33e0f80c1735d46a9090e120f5a468b7
Shane Honanie <shaneh425@gmail.com>
2018-12-31 13:24:13 -0800

    Background Image

--- rncourse/src/screens/Auth/Auth.js
+++ rncourse/src/screens/Auth/Auth.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet, ImageBackground } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
 import MainText from '../../components/UI/MainText/MainText';
+import backgroundImage from '../../assets/background.jpg';
 
 class AuthScreen extends Component {
 	loginHandler = () => {
@@ -13,18 +14,20 @@ class AuthScreen extends Component {
 
 	render() {
 		return (
-			<View style={styles.container}>
-				<MainText>
-					<HeadingText>Please Log In</HeadingText>
-				</MainText>
-				<Button title="Switch to Login" />
-				<View style={styles.inputContainer}>
-					<DefaultInput placeholder="Your E-Mail Address" style={styles.input} />
-					<DefaultInput placeholder="Password" style={styles.input} />
-					<DefaultInput placeholder="Confirm Password" style={styles.input} />
+			<ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+				<View style={styles.container}>
+					<MainText>
+						<HeadingText>Please Log In</HeadingText>
+					</MainText>
+					<Button title="Switch to Login" />
+					<View style={styles.inputContainer}>
+						<DefaultInput placeholder="Your E-Mail Address" style={styles.input} />
+						<DefaultInput placeholder="Password" style={styles.input} />
+						<DefaultInput placeholder="Confirm Password" style={styles.input} />
+					</View>
+					<Button title="Submit" onPress={this.loginHandler} style={{ backgroundColor: 'transparent' }} />
 				</View>
-				<Button title="Submit" onPress={this.loginHandler} />
-			</View>
+			</ImageBackground>
 		);
 	}
 }
@@ -34,6 +37,10 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center'
+	},
+	backgroundImage: {
+		width: '100%',
+		flex: 1
 	},
 	inputContainer: {
 		width: '80%'
