91503cb6dee72ae7d729d44bbb830df6ffb5ec51
Shane Honanie <shaneh425@gmail.com>
2019-01-01 18:01:42 -0800

    Platform Specific Icons

--- rncourse/src/components/PlaceInput/PlaceInput.js
+++ rncourse/src/components/PlaceInput/PlaceInput.js
@@ -3,7 +3,7 @@ import { View, TextInput, Button, StyleSheet } from 'react-native';
 
 import DefaultInput from '../UI/DefaultInput/DefaultInput';
 
-placeInput = props => (
+const placeInput = props => (
 	<DefaultInput placeholder="Place Name" value={props.placeName} onChangeText={props.onChangeText} />
 );
 
