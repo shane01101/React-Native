6404678caa761d8e3b8838dd97ff34f5c4d0a50d
Shane Honanie <shaneh425@gmail.com>
2019-01-07 13:58:21 -0800

    Map Marker

--- rncourse/src/components/PickLocation/PickLocation.js
+++ rncourse/src/components/PickLocation/PickLocation.js
@@ -9,7 +9,8 @@ class PickLocation extends Component {
 			longitude: -122.4013726,
 			latitudeDelta: 0.0122,
 			longitudeDelta: (Dimensions.get('window').width / Dimensions.get('window').height) * 0.0122
-		}
+		},
+		locationChosen: false
 	};
 
 	pickLocationHandler = event => {
@@ -20,12 +21,19 @@ class PickLocation extends Component {
 					...prevState.focusedLocation,
 					latitude: coords.latitude,
 					longitude: coords.longitude
-				}
+				},
+				locationChosen: true
 			};
 		});
 	};
 
 	render() {
+		let marker = null;
+
+		if (this.state.locationChosen) {
+			marker = <MapView.Marker coordinate={this.state.focusedLocation} />;
+		}
+
 		return (
 			<View style={styles.container}>
 				<MapView
@@ -33,7 +41,9 @@ class PickLocation extends Component {
 					region={this.state.focusedLocation}
 					style={styles.map}
 					onPress={this.pickLocationHandler}
-				/>
+				>
+					{marker}
+				</MapView>
 				<View style={styles.button}>
 					<Button title="Locate Me" onPress={() => alert('Pick Location')} />
 				</View>
