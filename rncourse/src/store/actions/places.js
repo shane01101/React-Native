0c64466da6ac529f259564233fabb7641ecab2c7
Shane Honanie <shaneh425@gmail.com>
2019-01-15 23:33:05 -0800

    storeImage is working!

--- rncourse/src/store/actions/places.js
+++ rncourse/src/store/actions/places.js
@@ -1,6 +1,7 @@
 import { SET_PLACES } from './actionTypes';
 import { uiStartLoading, uiStopLoading } from './index';
 
+//1/15 @ 11:32 storeImage uploads Image
 export const addPlace = (placeName, location, image) => {
 	console.log('image', image);
 	return dispatch => {
