48fc7a41d7d36be36fa965ae298cf1b738b4778e
Shane Honanie <shaneh425@gmail.com>
2019-01-18 16:23:41 -0800

    Signup Logic

--- rncourse/src/store/actions/auth.js
+++ rncourse/src/store/actions/auth.js
@@ -4,30 +4,25 @@ import startMainTabs from '../../screens/MainTabs/startMainTabs';
 
 export const tryAuth = (authData, authMode) => {
 	return dispatch => {
-		if (authMode === 'login') {
-		} else {
-			dispatch(authSignup(authData));
-		}
-	};
-};
-
-export const authSignup = authData => {
-	return dispatch => {
 		dispatch(uiStartLoading());
-		fetch(
-			'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBI0Yurd6LbQD2ru5eVOMths3wpHQtolK4',
-			{
-				method: 'POST',
-				body: JSON.stringify({
-					email: authData.email,
-					password: authData.password,
-					returnSecureToken: true
-				}),
-				headers: {
-					'Content-Type': 'application/json'
-				}
+		const apiKey = 'AIzaSyBI0Yurd6LbQD2ru5eVOMths3wpHQtolK4';
+		let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + apiKey;
+
+		if (authMode === 'signup') {
+			url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' + apiKey;
+		}
+
+		fetch(url, {
+			method: 'POST',
+			body: JSON.stringify({
+				email: authData.email,
+				password: authData.password,
+				returnSecureToken: true
+			}),
+			headers: {
+				'Content-Type': 'application/json'
 			}
-		)
+		})
 			.catch(err => {
 				console.log(err);
 				alert('Authentication failed, please try again!');
