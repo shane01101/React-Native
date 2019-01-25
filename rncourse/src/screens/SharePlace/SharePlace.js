dc66c63e25259c2e9ec2d12ec5a5509d1995b244
Shane Honanie <shaneh425@gmail.com>
2019-01-25 13:43:08 -0800

    Tab redirect after submit

--- rncourse/src/screens/SharePlace/SharePlace.js
+++ rncourse/src/screens/SharePlace/SharePlace.js
@@ -9,6 +9,7 @@ import HeadingText from '../../components/UI/HeadingText/HeadingText';
 import PickImage from '../../components/PickImage/PickImage';
 import PickLocation from '../../components/PickLocation/PickLocation';
 import validate from '../../utility/validation';
+import { startAddPlace } from '../../store/actions/index';
 
 class SharePlaceScreen extends Component {
 	static navigatorStyle = {
@@ -47,7 +48,20 @@ class SharePlaceScreen extends Component {
 		});
 	};
 
+	componentDidUpdate() {
+		if (this.props.placeAdded) {
+			this.props.navigator.switchToTab({ tabIndex: 0 });
+			//this.props.onStartAddPlace();
+		}
+	}
+
 	onNavigatorEvent = event => {
+		if (event.type === 'ScreenChangedEvent') {
+			if (event.id === 'willAppear') {
+				this.props.onStartAddPlace();
+			}
+		}
+
 		if (event.type === 'NavBarButtonPress') {
 			if (event.id === 'sideDrawerToggle') {
 				this.props.navigator.toggleDrawer({
@@ -171,13 +185,15 @@ const styles = StyleSheet.create({
 
 const mapStateToProps = state => {
 	return {
-		isLoading: state.ui.isLoading
+		isLoading: state.ui.isLoading,
+		placeAdded: state.places.placeAdded
 	};
 };
 
 const mapDispatchToProps = dispatch => {
 	return {
-		onAddPlace: (placeName, location, image) => dispatch(addPlace(placeName, location, image))
+		onAddPlace: (placeName, location, image) => dispatch(addPlace(placeName, location, image)),
+		onStartAddPlace: () => dispatch(startAddPlace())
 	};
 };
 
